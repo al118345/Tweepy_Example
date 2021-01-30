91efc42fbb33c180b7d706656ca76cd0a89b099a
Ruben Perez Ibañez <al118345@uji.es>
2021-01-30 20:40:42 +0100

    Create twint.py
    búsqueda  tweets antiguos

---
+++ twint.py
@@ -0,0 +1,22 @@
+import twint
+
+# Configure buscamos todos los tweets emitidos por nosotros en angular.
+c = twint.Config()
+c.Username = "1938web"
+c.Search = "Angular"
+
+# Ejecutamos la búsquedas.
+twint.run.Search(c)
+
+
+# Segunda consulta.
+c = twint.Config()
+# Buscamos un usario
+c.Username = "javieroliveira_"
+# Buscamos el término que aparezca.
+c.Search = "joan"
+#almacenamos la búsquead en un documento csv.
+c.Store_csv = True
+# escoge el nombre del fichero.
+c.Output = "joanplanas.csv"
+twint.run.Search(c)
